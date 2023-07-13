f547c3c6f2e38494075350c72a197ea557a9eeaf
Oliver Li <li.oliver.ofl@gmail.com>
2023-07-13 15:37:43 -0400

    Initial Commit

---
+++ Drum App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drum App
+//
+//  Created by Oliver Li on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drum App/Drum_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drum_AppApp.swift
+//  Drum App
+//
+//  Created by Oliver Li on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Drum_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
